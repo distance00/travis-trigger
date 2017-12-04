ba9b3001c340278c12aed7f275d7d26c778839a4
phuslu <phuslu@hotmail.com>
2017-12-04 23:36:37 +0800

    move to bitbucket.org

--- bump.sh
+++ bump.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-GITHUB_TOKEN=$(cat /home/phuslu/.GITHUB_TOKEN)
-CURRENT_COMMIT=$(git ls-remote https://phuslu:${GITHUB_TOKEN}@github.com/phuslu/promvps | grep refs/heads/master | awk '{print $1}')
+BITBUCKET_PASSWORD="$(cat /home/phuslu/.BITBUCKET_PASSWORD)"
+CURRENT_COMMIT=$(git ls-remote "https://phuslu:${BITBUCKET_PASSWORD}@bitbucket.org/phuslu/promvps" | grep refs/heads/master | awk '{print $1}')
 ORIGINAL_COMMIT=$(git log -1 --format="%s %b" | grep -oE '[0-9a-z]{40}' | tail -1)
 
 if [ "${CURRENT_COMMIT}" = "${ORIGINAL_COMMIT}" ]; then
 	exit 0
 fi
 
-COMMIT_MESSAGE=$(curl -sSL -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/phuslu/promvps/commits/${CURRENT_COMMIT} | python -c 'import sys,json; print(json.load(sys.stdin)["commit"]["message"])')
+COMMIT_MESSAGE=$(curl -v -u "phuslu:${BITBUCKET_PASSWORD}" "https://api.bitbucket.org/2.0/repositories/phuslu/promvps/commits/master"|python -c 'import sys,json; print(json.load(sys.stdin)["values"][0]["message"])')
 
 git commit --amend --allow-empty -m "${COMMIT_MESSAGE}
 
